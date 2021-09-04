472a03ddee5c87b1fe602f5752bcfbe10afc10b5
mitesh <mitesh.wininfoway@gmail.com>
2021-09-04 13:32:38 +0530

    status check

---
+++ application/controllers/api/CronServiceStatus.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('BASEPATH'))exit('No direct script access allowed');
+
+class CronServiceStatus extends CI_Controller {
+
+    public function __construct() 
+	{
+        parent::__construct();
+    }
+    public function  start()
+	{
+		sleep(delay_time);		
+		while(1)
+		{
+			$this->index();
+			sleep(delay_time);
+		}		
+	}
+ 	public function  index()
+	{
+		$status = shell_exec('systemctl is-failed CronLiveSync.service');
+		if(trim($status)){
+			shell_exec('systemctl restart CronLiveSync.service');	
+		}		
+	}
+	
+
+}
+?>
