bc4838f0ca66e380efcb26e8abc072c3f8b49816
mitesh <mitesh.wininfoway@gmail.com>
2021-09-04 11:19:15 +0530

    db changes

--- database/local_anpr_yard_web.sql
+++ database/local_anpr_yard_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2021 at 07:23 AM
+-- Generation Time: Sep 04, 2021 at 07:48 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.29
 
@@ -51,51 +51,25 @@ CREATE TABLE `car_plates` (
   `driver_name` varchar(255) DEFAULT NULL,
   `driver_mobile_no` varchar(20) DEFAULT NULL,
   `company_name` varchar(255) DEFAULT NULL,
-  `load_number` varchar(255) DEFAULT NULL,
-  `type` enum('Route','Bulk','') DEFAULT NULL,
   `vehicle_type` varchar(255) DEFAULT NULL,
   `site_entry` datetime DEFAULT NULL,
   `site_exit` datetime DEFAULT NULL,
-  `exit` int(1) NOT NULL DEFAULT 0,
+  `exit` int(11) NOT NULL DEFAULT 0,
   `parked` int(1) NOT NULL DEFAULT 0,
   `reparked` int(11) NOT NULL,
-  `induction` varchar(255) DEFAULT NULL,
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_date` datetime NOT NULL,
-  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   `is_sync` tinyint(1) NOT NULL DEFAULT 0,
-  `net` int(11) NOT NULL DEFAULT 0,
-  `sent_email` tinyint(1) NOT NULL,
-  `entry_approved` tinyint(1) NOT NULL DEFAULT 0,
   `trailer_rego` varchar(100) DEFAULT NULL,
-  `download_image` tinyint(1) NOT NULL DEFAULT 1,
-  `entry_approved_datetime` datetime DEFAULT NULL,
-  `exit_approved` tinyint(1) NOT NULL DEFAULT 0,
-  `exit_approved_datetime` datetime DEFAULT NULL,
-  `manully_approved` tinyint(1) NOT NULL DEFAULT 0,
-  `is_image_deleted` int(11) NOT NULL DEFAULT 0,
-  `invalid_plate` int(11) NOT NULL DEFAULT 0,
-  `invalid_type` varchar(10) NOT NULL,
-  `reason` varchar(200) NOT NULL,
-  `is_black_white_list` tinyint(1) NOT NULL DEFAULT 0,
-  `manully_entry` tinyint(4) NOT NULL DEFAULT 0,
   `manully_exit` tinyint(4) NOT NULL DEFAULT 0,
   `car_plates_id` int(10) NOT NULL,
-  `is_sent_overstay_email` tinyint(1) NOT NULL DEFAULT 0,
-  `added_by` varchar(50) NOT NULL,
   `rapid_id` int(11) DEFAULT NULL,
   `order_number` varchar(250) DEFAULT NULL,
   `cie_id` int(11) DEFAULT NULL,
-  `driver_dec_status` int(11) NOT NULL DEFAULT -1,
-  `driver_dec_date` datetime DEFAULT NULL,
-  `question_answer` text NOT NULL,
-  `drivers_dec` int(11) NOT NULL DEFAULT 0,
-  `operator_question` text NOT NULL,
-  `visium_status` varchar(512) NOT NULL,
-  `gate_name` varchar(50) NOT NULL,
-  `match_photo` int(11) NOT NULL DEFAULT 0,
   `vehicle_image` text DEFAULT NULL,
-  `driver_image` text DEFAULT NULL
+  `drivers_dec` int(11) NOT NULL DEFAULT 0,
+  `driver_image` text DEFAULT NULL,
+  `live_sync` int(11) NOT NULL DEFAULT 0
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
