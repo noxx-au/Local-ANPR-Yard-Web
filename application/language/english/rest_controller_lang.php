6af52005b0eb8a447168309c3039265649a5a958
vaibhavnoxx <vaibhav@noxx.com.au>
2021-09-07 08:43:19 +0530

    fix issue

--- application/language/english/rest_controller_lang.php
+++ application/language/english/rest_controller_lang.php
@@ -5,9 +5,9 @@
  */
 
 $lang['text_rest_invalid_api_key'] = 'Invalid API key %s'; // %s is the REST API key
-$lang['text_rest_invalid_credentials'] = 'Invalid credentials';
-$lang['text_rest_ip_denied'] = 'IP denied';
-$lang['text_rest_ip_unauthorized'] = 'IP unauthorized';
+$lang['text_rest_invalid_credentials'] = 'Invalid Credentials';
+$lang['text_rest_ip_denied'] = 'IP Denied';
+$lang['text_rest_ip_unauthorized'] = 'IP Unauthorized';
 $lang['text_rest_unauthorized'] = 'Unauthorized';
 $lang['text_rest_ajax_only'] = 'Only AJAX requests are allowed';
 $lang['text_rest_api_key_unauthorized'] = 'This API key does not have access to the requested controller';
@@ -15,5 +15,5 @@ $lang['text_rest_api_key_permissions'] = 'This API key does not have enough perm
 $lang['text_rest_api_key_time_limit'] = 'This API key has reached the time limit for this method';
 $lang['text_rest_ip_address_time_limit'] = 'This IP Address has reached the time limit for this method';
 $lang['text_rest_unknown_method'] = 'Unknown method';
-$lang['text_rest_unsupported'] = 'Unsupported protocol';
-$lang['text_rest_ip_toke_not_valid'] = 'sorry, token is not valid';
+$lang['text_rest_unsupported'] = 'Unsupported Protocol';
+$lang['text_rest_ip_toke_not_valid'] = 'Sorry, token is not valid';
